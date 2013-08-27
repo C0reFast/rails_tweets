75e8f944f2524bf35a496ea81048147f9b1ea3ac
c0refast <topchenfu@qq.com>
2013-08-27 09:42:13 +0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,16 @@ RailsTweets::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destory', via: 'delete'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   resources :sessions, only:[:new, :create, :destroy]
   resources :microposts, only:[:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
